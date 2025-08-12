586e801e8b2c3d23a035c83dbd3cb44d695d2449
swanboats <wildcandy0273@outlook.jp>
2025-08-12 14:53:36 +0900

    git commit -m my_blog_project

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,12 @@
-from django.db import models
-
-# Create your models here.
-
+# blog/models.py
 from django.db import models
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)  # 記事のタイトル
-    content = models.TextField()              # 記事の内容
+    title = models.CharField(max_length=200)  # 記事タイトル
+    content = models.TextField()              # 記事本文
     created_at = models.DateTimeField(auto_now_add=True)  # 作成日時
     updated_at = models.DateTimeField(auto_now=True)      # 更新日時
 
     def __str__(self):
         return self.title
+
