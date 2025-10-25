8a39e7184718120a9649fb138c918c4cab8dca8f
swanboats <wildcandy0273@outlook.jp>
2025-10-25 13:43:44 +0900

    メッセージ

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Post, SiteSetting
+from .models import Post, SiteSetting, Profile
 
-admin.site.register(Post)
-admin.site.register(SiteSetting)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+    search_fields = ('title', 'content')
+
+@admin.register(SiteSetting)
+class SiteSettingAdmin(admin.ModelAdmin):
+    list_display = ('site_title',)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('name',)
